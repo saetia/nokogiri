50f4e43cfbbf92c5b2074a120587b484bd99fb88
Joel Glovacki <saetia@gmail.com>
2013-08-15 11:33:05 -0500

    made the syntax more like the original ruby version

--- nokogiri.php
+++ nokogiri.php
@@ -1,138 +1,121 @@
-<?php
+<?
 
-/*
- * This file is part of the zero package.
- * Copyright (c) 2012 olamedia <olamedia@gmail.com>
- *
- * This source code is release under the MIT License.
- * 
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
-/**
- * Simple HTML parser
- *
- * @author olamedia <olamedia@gmail.com>
- */
-class nokogiri implements IteratorAggregate{
+class Nokogiri implements IteratorAggregate, ArrayAccess {
+	
 	const
 	regexp = 
 	"/(?P<tag>[a-z0-9]+)?(\[(?P<attr>\S+)=(?P<value>[^\]]+)\])?(#(?P<id>[^\s:>#\.]+))?(\.(?P<class>[^\s:>#\.]+))?(:(?P<pseudo>(first|last|nth)-child)(\((?P<expr>[^\)]+)\))?)?\s*(?P<rel>>)?/isS"
 	;
-	protected $_source = '';
-	/**
-	 * @var DOMDocument
-	 */
-	protected $_dom = null;
-	/**
-	 * @var DOMDocument
-	 */
-	protected $_tempDom = null;
-	/**
-	 * @var DOMXpath
-	 * */
-	protected $_xpath = null;
+	
+	public $dom;
+	public $xpath;
+	public $nodes;
+	public $current_node;
+	
 	protected static $_compiledXpath = array();
-	public function __construct($htmlString = ''){
-		$this->loadHtml($htmlString);
+	
+	public function __construct(){
+		$this->dom = new DOMDocument();
+		$this->dom->preserveWhiteSpace = false;
 	}
-	public function getRegexp(){
-		$tag = "(?P<tag>[a-z0-9]+)?";
-		$attr = "(\[(?P<attr>\S+)=(?P<value>[^\]]+)\])?";
-		$id = "(#(?P<id>[^\s:>#\.]+))?";
-		$class = "(\.(?P<class>[^\s:>#\.]+))?";
-		$child = "(first|last|nth)-child";
-		$expr = "(\((?P<expr>[^\)]+)\))";
-		$pseudo = "(:(?P<pseudo>".$child.")".$expr."?)?";
-		$rel = "\s*(?P<rel>>)?";
-		$regexp = "/".$tag.$attr.$id.$class.$pseudo.$rel."/isS";
-		return $regexp;
+	
+	public function attributes(){
+		$attributes = array();
+		foreach ($this->current_node->attributes as $attribute) $attributes[$attribute->nodeName] = $attribute->nodeValue;
+		return $attributes;
 	}
-	public static function fromHtml($htmlString){
-		$me = new self();
-		$me->loadHtml($htmlString);
-		return $me;
-	}
-	public static function fromHtmlNoCharset($htmlString){
-		$me = new self();
-		$me->loadHtmlNoCharset($htmlString);
-		return $me;
-	}
-	public static function fromDom($dom){
-		$me = new self();
-		$me->loadDom($dom);
-		return $me;
-	}
-	public function loadDom($dom){
-		$this->_dom = $dom;
-	}
-	public function loadHtmlNoCharset($htmlString = ''){
-		$dom = new DOMDocument('1.0', 'UTF-8');
-		$dom->preserveWhiteSpace = false;
-		if (strlen($htmlString)){
-			libxml_use_internal_errors(true);
-			$dom->loadHTML('<?xml encoding="UTF-8">'.$htmlString);
-			// dirty fix
-			foreach ($dom->childNodes as $item){
-			    if ($item->nodeType == XML_PI_NODE){
-			        $dom->removeChild($item); // remove hack
-			        break;
-			    }
-			}
-			$dom->encoding = 'UTF-8'; // insert proper
-			libxml_clear_errors();
-		}
-		$this->loadDom($dom);
-	}
-	public function loadHtml($htmlString = ''){
-		$dom = new DOMDocument('1.0', 'UTF-8');
-		$dom->preserveWhiteSpace = false;
-		if (strlen($htmlString)){
-			libxml_use_internal_errors(true);
-			$dom->loadHTML($htmlString);
-			libxml_clear_errors();
-		}
-		$this->loadDom($dom);
-	}
-	function __invoke($expression){
-		return $this->get($expression);
-	}
-	public function get($expression, $compile = true){
-		/*if (strpos($expression, ' ') !== false){
-			$a = explode(' ', $expression);
-			foreach ($a as $k=>$sub){
-				$a[$k] = $this->getXpathSubquery($sub);
-			}
-			return $this->getElements(implode('', $a));
-		}*/
-		return $this->getElements($this->getXpathSubquery($expression, false, $compile));
-	}
-	protected function getNodes(){
 
+	public function offsetExists($key){
+		return !!!$this->current_node->getAttribute($key);
 	}
-	public function getDom(){
-		if ($this->_dom instanceof DOMDocument){
-			return $this->_dom;
-		}elseif ($this->_dom instanceof DOMNodeList){
-			if ($this->_tempDom === null){
-				$this->_tempDom = new DOMDocument('1.0', 'UTF-8');
-				$root = $this->_tempDom->createElement('root');
-				$this->_tempDom->appendChild($root);
-				foreach ($this->_dom as $domElement){
-					$domNode = $this->_tempDom->importNode($domElement, true);
-					$root->appendChild($domNode);
-				}
-			}
-			return $this->_tempDom;
+	public function offsetGet($key){
+		return $this->current_node->getAttribute($key);
+	}
+	public function offsetSet($key, $value){
+		$this->current_node->setAttribute($key,$value);
+		return $this;
+	}
+	public function offsetUnset($key){
+		$this->current_node->removeAttribute($key);
+		return $this;
+	}
+	
+	public function __get($method){
+		switch($method){
+
+			case "text":
+				return ($this->current_node) ? $this->current_node->textContent : null;
+			break;
+
+			case "content":
+				return ($this->current_node) ? $this->current_node->nodeValue : null;
+			break;
+			
+			case "root":
+				
+			break;
+			
 		}
 	}
-	protected function getXpath(){
-		if ($this->_xpath === null){
-			$this->_xpath = new DOMXpath($this->getDom());
+	
+	public function __set($method, $args){
+		
+		switch($method){
+			case "content":
+				$this->current_node->nodeValue = $args;
+			break;
 		}
-		return $this->_xpath;
+		
+		return $this;
+	
 	}
+
+	
+	public function text($regex=null){
+		if (is_null($regex)) return $this->text;
+		preg_match_all($regex, $this->text, $matches);
+		return (count($matches[0])) ? $matches[0][0] : null;
+	}
+	
+	
+	/*
+	$frag = $dom->createDocumentFragment();
+	$frag->appendXML('<h1>foo</h1>');
+	$el->appendChild($frag);
+	*/
+	
+	
+	
+	public function from_html($html){
+		libxml_use_internal_errors(true);
+		$this->dom->loadHTML($html);
+	  $this->xpath = new DOMXpath($this->dom);
+		libxml_clear_errors();
+		return $this;
+	}
+	
+	public function delete(){
+		return $this->remove();
+	}
+	
+	public function remove(){
+		foreach ($this->nodes as $node)
+			$node->parentNode->removeChild($node);
+		return $this;
+	}
+	
+	public function __toString(){
+		return json_encode($this->to_array());
+	}
+	
+	
+	public function get_elements($query){
+	  $this->nodes = $this->xpath->query($query);
+	  if ($this->nodes === false) throw new Exception('Malformed xpath');
+	  return $this;
+	}
+	
+	
 	public function getXpathSubquery($expression, $rel = false, $compile = true){
 		if ($compile){
 			$key = $expression.($rel?'>':'*');
@@ -191,28 +174,134 @@ class nokogiri implements IteratorAggregate{
 		}
 		return $query;
 	}
-	protected function getElements($xpathQuery){
-		if (strlen($xpathQuery)){
-			$nodeList = $this->getXpath()->query($xpathQuery);
-			if ($nodeList === false){
-				throw new Exception('Malformed xpath');
-			}
-			return self::fromDom($nodeList);
+	
+	
+	
+	
+
+	public function tag($query){
+		$this->nodes = $this->dom->getElementsByTagName($query);
+		return $this->nodes;
+	}
+
+	public function id($query){
+		$this->nodes = $this->dom->getElementsById($query);
+		return $this->nodes;
+	}
+
+
+	public function css($query){
+		$this->search($query);
+		return $this->nodes;
+	}
+	
+	public function xpath($query){
+		$this->get_elements($query);
+		return $this->nodes;
+	}
+	
+	public function search($query, $compile=true){
+		$this->get_elements($this->getXpathSubquery($query, false, $compile));
+		return $this->nodes;
+	}
+	
+	//return first element
+	public function at_css($query){
+		$this->current_node = $this->search($query)->item(0);
+		return $this;
+	}
+
+	//return first element
+	public function at_xpath($query){
+		$this->current_node = $this->xpath($query)->item(0);
+		return $this->current_node;
+	}
+	
+	public function to_html($node=null){
+
+		if ($node instanceof Nokogiri){
+			return $this->as_fragment($this->dom->saveHTML($node->current_node));
 		}
+		
+		return $this->as_fragment($this->dom->saveHTML($node));
+		
 	}
-	public function toXml(){
-		return $this->getDom()->saveXML();
+
+	protected function as_fragment($html){
+		return preg_replace(array("/^\<\!DOCTYPE.*?<html><body>/si", "!</body></html>$!si"), '', $html);
 	}
-	public function toArray($xnode = null){
+	
+
+	public function to_inner_html($node=null){
+		
+		if ($node instanceof Nokogiri){
+		
+			$html = '';
+	
+			if (!property_exists($node->current_node, 'childNodes')) {
+				foreach ($node->current_node as $node)
+					$html .= $this->to_inner_html($node);
+				return $html;
+			}
+			
+			foreach($this->current_node->childNodes as $node)
+			   $html .= $this->to_html($node);
+			
+			return $html;
+		
+		}
+		
+		return $this->to_html($this->dom->getElementsByTagName('html')->item(0));
+		
+	}
+
+
+
+	public function to_xml(){
+		
+	}
+
+	public function add_next_sibling(){
+		
+	}
+	
+	public function name(){
+		
+	}
+	
+	public function parent(){
+		
+	}
+	
+	public function content(){
+		
+	}
+	
+	public function wrap(){
+		
+	}
+
+	public static function HTML($html){
+		$dom = new self();
+		$dom->from_html($html);
+		return $dom;
+	}
+	
+	public static function XML($xml){
+
+	}
+
+
+	public function to_array($xnode = null){
 		$array = array();
 		if ($xnode === null){
-			if ($this->_dom instanceof DOMNodeList){
-				foreach ($this->_dom as $node){
-					$array[] = $this->toArray($node);
+			if ($this->nodes instanceof DOMNodeList){
+				foreach ($this->nodes as $node){
+					$array[] = $this->to_array($node);
 				}
 				return $array;
 			}
-			$node = $this->getDom();
+			$node = $this->dom;
 		}else{
 			$node = $xnode;
 		}
@@ -226,7 +315,7 @@ class nokogiri implements IteratorAggregate{
 		}
 		if ($node->hasChildNodes()){
 			foreach ($node->childNodes as $childNode){
-				$array[$childNode->nodeName][] = $this->toArray($childNode);
+				$array[$childNode->nodeName][] = $this->to_array($childNode);
 			}
 		}
 		if ($xnode === null){
@@ -234,16 +323,71 @@ class nokogiri implements IteratorAggregate{
 		}
 		return $array;
 	}
+	
+	
 	public function getIterator(){
-		$a = $this->toArray();
+		$a = $this->to_array();
 		return new ArrayIterator($a);
 	}
+
 }
 
 
-/*$saw = new nokogiri();
-echo $saw->getXpathSubquery('#boo #ge > #id:nth-child(3n+5)');
-echo "\r\n";*/
 
 
 
+
+
+
+
+
+
+
+
+
+/*
+[0] => getAttribute
+[1] => setAttribute
+[2] => removeAttribute
+[3] => getAttributeNode
+[4] => setAttributeNode
+[5] => removeAttributeNode
+[6] => getElementsByTagName
+[7] => getAttributeNS
+[8] => setAttributeNS
+[9] => removeAttributeNS
+[10] => getAttributeNodeNS
+[11] => setAttributeNodeNS
+[12] => getElementsByTagNameNS
+[13] => hasAttribute
+[14] => hasAttributeNS
+[15] => setIdAttribute
+[16] => setIdAttributeNS
+[17] => setIdAttributeNode
+[18] => __construct
+[19] => insertBefore
+[20] => replaceChild
+[21] => removeChild
+[22] => appendChild
+[23] => hasChildNodes
+[24] => cloneNode
+[25] => normalize
+[26] => isSupported
+[27] => hasAttributes
+[28] => compareDocumentPosition
+[29] => isSameNode
+[30] => lookupPrefix
+[31] => isDefaultNamespace
+[32] => lookupNamespaceUri
+[33] => isEqualNode
+[34] => getFeature
+[35] => setUserData
+[36] => getUserData
+[37] => getNodePath
+[38] => getLineNo
+[39] => C14N
+[40] => C14NFile
+*/
+
+
+?>
